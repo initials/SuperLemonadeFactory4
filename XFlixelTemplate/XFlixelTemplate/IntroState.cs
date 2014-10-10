b6d1ebc2bc6fcbe9730d30d92d4c5de338db18cf
Shane Brouwer <shane@findesign.com.au>
2014-10-10 11:06:47 +1100

    x

---
+++ XFlixelTemplate/XFlixelTemplate/IntroState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using org.flixel;
+
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SuperLemonadeFactory4
+{
+    public class IntroState : FlxState
+    {
+
+        override public void create()
+        {
+            FlxG.backColor = Color.Purple;
+            base.create();
+
+
+
+        }
+
+        override public void update()
+        {
+
+
+
+
+            base.update();
+        }
+
+
+    }
+}
